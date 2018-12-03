fc33da000f4939e01cd81eff06ce9994a824f527
wtaolin <wtaolin@mail.ustc.edu.cn>
2018-12-03 23:49:02 +0800

    http get function test

--- main/main.go
+++ main/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"Demo/factory"
+	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
 	"sync"
 	"time"
 )
@@ -19,20 +24,41 @@ type bb struct {
 type name int
 
 const (
-	HHH name=iota
+	HHH name = iota
 	eee
 	hehe
 	hehh
-
 )
+
 type element struct {
 	a int
 	b int
 }
-func main() {
-//	factory.Json2Struct()
-	//factory.GenerateSetAndGetMethod()
 
-	queue:=&factory.BufferQueueImp{}
-	factory.TestReflecType(queue)
+func main() {
+
+}
+
+func httpGetTest() {
+	url := "http://www.w3school.com.cn/example/html_examples.asp"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln("get rsp err")
 	}
+	if resp.StatusCode != 200 {
+		log.Fatalln("statusCode is wrong")
+	}
+	str, _ := filepath.Abs(".")
+	file, err := os.Create(str + "/text.txt")
+	if err != nil {
+		log.Fatalln("create file failed")
+	}
+	defer file.Close()
+	dst := io.MultiWriter(os.Stdout, file)
+	written, err := io.Copy(dst, resp.Body)
+	fmt.Println(written)
+	if err := resp.Body.Close(); err != nil {
+		log.Fatalln("resp.body close failed")
+	}
+	//ioutil.WriteFile(str+"/text.txt",[]byte(resp),0777)
+}
